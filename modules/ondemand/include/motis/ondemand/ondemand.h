1a2d6009d616ad542d1acaee0a45a489f2f2a88d
NeodaTech <Joshua.Moell@t-online.de>
2023-02-01 20:15:32 +0100

    add osrm request

--- modules/ondemand/include/motis/ondemand/ondemand.h
+++ modules/ondemand/include/motis/ondemand/ondemand.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "motis/module/module.h"
+#include "motis/protocol/Message_generated.h"
+#include "geo/latlng.h"
 
 namespace motis::ondemand {
 
@@ -14,6 +17,11 @@ public:
 private:
   motis::module::msg_ptr calculate(motis::module::msg_ptr const&);
   motis::module::msg_ptr add_taxi(motis::module::msg_ptr const&);
+
+  motis::module::msg_ptr make_osrm_request(
+          Position const* pos, std::vector<Position> const* destinations,
+          std::string const& profile, SearchDir direction);
+  //motis::module::msg_ptr make_geo_request(geo::latlng const& pos, double radius);
 };
 
 }  // namespace motis::intermodal
--- modules/ondemand/src/ondemand.cc
+++ modules/ondemand/src/ondemand.cc
@@ -2,9 +2,13 @@
 
 #include "motis/core/common/logging.h"
 #include "motis/protocol/Message_generated.h"
+#include "motis/core/conv/position_conv.h"
+#include "motis/module/context/motis_call.h"
+#include "motis/module/message.h"
 
 using namespace flatbuffers;
 using namespace motis::module;
+using namespace motis::osrm;
 using motis::logging::info;
 
 namespace motis::ondemand {
@@ -39,10 +43,41 @@ namespace motis::ondemand {
         LOG(info) << "calculate";
         auto req = motis_content(OndemandRequest, msg);
         LOG(info) << "ondemand - calculate - OndemandRequest - Input - " << req->RequestTime();
+
+        //make_geo_request(); // Request to get Geo Locations ?
+        Position start_pos{req->InputStartPosition()->lat(), req->InputStartPosition()->lng()};
+        std::vector<Position> destinations;
+        destinations.emplace_back(req->InputDestinationPosition()->lat(), req->InputDestinationPosition()->lng());
+
+        //make_osrm_request(start_pos, parkings, "car", SearchDir_Backward));
+        //make_osrm_request(start_pos, parkings, "car", SearchDir_Forward));
+        auto const osrm_msg = motis_call(make_osrm_request(&start_pos, &destinations, "car", SearchDir_Backward))->val();
+        auto const osrm_resp = motis_content(OSRMOneToManyResponse, osrm_msg);
+
+        auto const dur = osrm_resp->costs()->Get(0)->duration();
+        auto const dist = osrm_resp->costs()->Get(0)->distance();
+
+        LOG(info) << "duration: " << dur;
+        LOG(info) << "distance: " << dist;
+
         message_creator mc;
         mc.create_and_finish(
             MsgContent_OndemandResponse,
             CreateOndemandResponse(mc, mc.CreateString("Ondemand Test response")).Union());
         return make_msg(mc);
     }
+
+    motis::module::msg_ptr ondemand::make_osrm_request(
+            Position const* pos, std::vector<Position> const* destinations,
+            std::string const& profile, SearchDir direction) {
+        message_creator mc;
+        mc.create_and_finish(
+                MsgContent_OSRMOneToManyRequest,
+                CreateOSRMOneToManyRequest(mc, mc.CreateString(profile), direction,
+                                           pos, mc.CreateVectorOfStructs(*destinations))
+                        .Union(),
+                "/osrm/one_to_many");
+        return make_msg(mc);
+    }
+
 }  // namespace motis::ondemand
